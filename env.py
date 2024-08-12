5dcb7756b16e76b41a282d266a1eae6a3e4f9a6f
Chlakhna <143691861+Chlakhna@users.noreply.github.com>
2024-08-12 13:31:02 +0700

    Update env.py

--- env.py
+++ env.py
@@ -23,12 +23,14 @@ from docx2pdf import convert
 # import pythoncom
 from io import BytesIO
 from dotenv import load_dotenv
+import requests
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Set your OpenAI API key from environment variable
-openai.api_key = os.getenv('OPENAI_API_KEY')
+# openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Set your Telegram bot token and chat ID
 telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
